87a38dfde976e6ee2cfb362853a6bdee4c8e89b0
a19335850 <SOZenkovich@sberbank.ru>
2022-04-26 12:31:33 +0700

    test

--- src/exporter.js
+++ src/exporter.js
@@ -1,2 +1,4 @@
-exports.func = require("./func");
-exports.getLastItem = require("./index");
+const {func} = require("./func");
+const {getLastItem} = require("./index");
+
+module.exports = {func, getLastItem};
